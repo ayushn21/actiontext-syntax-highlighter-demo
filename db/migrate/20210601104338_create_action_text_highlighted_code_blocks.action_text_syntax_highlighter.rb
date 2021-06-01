d20c0ba80dd620fe5e65394f463b1af84cb8c475
Ayush Newatia <ayush@hey.com>
2021-06-01 11:47:11 +0100

    Initial commit

---
+++ db/migrate/20210601104338_create_action_text_highlighted_code_blocks.action_text_syntax_highlighter.rb
@@ -0,0 +1,12 @@
+# This migration comes from action_text_syntax_highlighter (originally 20210531195232)
+class CreateActionTextHighlightedCodeBlocks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :action_text_highlighted_code_blocks do |t|
+      t.text :content, size: :medium
+      t.string :language
+      t.references :rich_text, foreign_key: { to_table: :action_text_rich_texts, on_delete: :cascade }
+
+      t.timestamps
+    end
+  end
+end
